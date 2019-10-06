31000a8ea56f438b3281bb75f4083c2764f9a449
Yashaswi Bhardwaj <meyash14@gmail.com>
2019-10-06 13:30:38 +0530

    seventh

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,6 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+           // console.log(body.daily.data[0])
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
